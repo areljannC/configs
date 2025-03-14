a483ca6cc7542640e17cedafcffb57f075bc1e2c
AJ Clemente <areljann.c@gmail.com>
2025-03-14 11:42:55 -0400

    Add a basic config for nvim

---
+++ nvim/init.lua
@@ -0,0 +1,40 @@
+-- enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- enable syntax highlighting and colors
+vim.opt.termguicolors = true
+
+-- enable smart indenting
+vim.opt.smartindent = true
+
+-- enable mouse support
+vim.opt.mouse = "a"
+
+-- disable swap files (use undo history instead)
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+-- enable incremental search with highlighting
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- better scrolling
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+-- reduce update time for responsiveness
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 400
+
+-- enable persistent undo history??
+
+-- auto-update file when changed externally
+vim.opt.autoread = true
+
+-- highlight the current cursor line
+vim.opt.cursorline = true
+
+-- display whitespace characters
+vim.opt.list = true
+vim.opt.listchars = { tab = ">> ", space = " ", trail = "." }
